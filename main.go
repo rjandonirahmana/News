59978452d908f86116137610c86080e2571293e8
rjandonirahmana <rjandonirahmana@gmail.com>
2021-11-22 12:34:58 +0700

    mongo update

--- main.go
+++ main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"net/http"
+	"time"
 
-	// "net/http"
-	// "time"
-
-	// "github.com/gorilla/mux"
-	// "github.com/rjandonirahmana/news/auth"
+	"github.com/gorilla/mux"
+	"github.com/rjandonirahmana/news/auth"
 	"github.com/rjandonirahmana/news/database"
-	// "github.com/rjandonirahmana/news/handler"
+	"github.com/rjandonirahmana/news/handler"
 	"github.com/rjandonirahmana/news/repository"
-	// "github.com/rjandonirahmana/news/route"
-	// "github.com/rjandonirahmana/news/usecase"
-	// "github.com/rs/cors"
+	"github.com/rjandonirahmana/news/route"
+	"github.com/rjandonirahmana/news/usecase"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -26,59 +24,51 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// authentication := auth.NewAuth("coba", "test")
+	authentication := auth.NewAuth("coba", "test")
 
-	// repoNews := repository.NewRepoNews(dbmongo)
-	// servicenews := usecase.NewServiceNews(repoNews)
-	// handlerNews := handler.NewHanlderNews(servicenews)
+	repoNews := repository.NewRepoNews(dbmongo)
+	servicenews := usecase.NewServiceNews(repoNews)
+	handlerNews := handler.NewHanlderNews(servicenews)
 
-	// repoUser := repository.NewRepoUser(dbmongo)
-	// serviceUser := usecase.NewUsecCaseUser(repoUser, "secret")
-	// handlerUser := handler.NewHandlerUser(serviceUser, authentication)
+	repoUser := repository.NewRepoUser(dbmongo)
+	serviceUser := usecase.NewUsecCaseUser(repoUser, "secret")
+	handlerUser := handler.NewHandlerUser(serviceUser, authentication)
 
-	empat := "5"
-	satu := "1"
 	repoComment := repository.NewRepoComments(dbmongo)
-	comment, err := repoComment.GetOneComment(&empat, &satu, context.Background())
+	serviceComment := usecase.NewUseCaseComent(repoComment)
+	handlerComment := handler.NewCommentHandler(serviceComment)
 
-	if err != nil {
-		fmt.Println(err)
+	repoAdmin := repository.NewAdminRepo(dbmongo)
+	serviceAdmin := usecase.NewAdminUsecase(repoAdmin, "SECRETBANGET")
+	handlerAdmin := handler.NewAdminHandler(serviceAdmin, authentication)
+
+	middleWare := handler.NewMiddleWare(authentication, serviceUser, repoAdmin)
+
+	r := mux.NewRouter()
+	route.NewsRoute(r, middleWare, handlerNews)
+	route.RouteAdmin(r, handlerAdmin)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"127.0.0.1:7000"},
+		AllowedMethods: []string{"POST", "PUT", "GET", "DELETE", "PATCH"},
+		AllowedHeaders: []string{"X-Access-Token", "Accept-Language", "Content-Type", "Content-Language", "Origin"},
+	})
+
+	r.HandleFunc("/create/comment", middleWare.AuthenticationUser(handlerComment.CreateComment)).Methods("PUT")
+	r.HandleFunc("/like/comment", middleWare.AuthenticationUser(handlerComment.LikeComment)).Methods("PUT")
+	r.HandleFunc("/register", handlerUser.Register).Methods("POST")
+	r.HandleFunc("/login", handlerUser.Login).Methods("POST")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:7000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
 	}
 
-	fmt.Println(comment)
-	// serviceComment := usecase.NewUseCaseComent(repoComment)
-	// handlerComment := handler.NewCommentHandler(serviceComment)
+	handlerCors := c.Handler(r)
 
-	// repoAdmin := repository.NewAdminRepo(dbmongo)
-	// serviceAdmin := usecase.NewAdminUsecase(repoAdmin, "SECRETBANGET")
-	// handlerAdmin := handler.NewAdminHandler(serviceAdmin, authentication)
-
-	// middleWare := handler.NewMiddleWare(authentication, serviceUser, repoAdmin)
-
-	// r := mux.NewRouter()
-	// route.NewsRoute(r, middleWare, handlerNews)
-	// route.RouteAdmin(r, handlerAdmin)
-
-	// c := cors.New(cors.Options{
-	// 	AllowedOrigins: []string{"127.0.0.1:7000"},
-	// 	AllowedMethods: []string{"POST", "PUT", "GET", "DELETE", "PATCH"},
-	// 	AllowedHeaders: []string{"X-Access-Token", "Accept-Language", "Content-Type", "Content-Language", "Origin"},
-	// })
-
-	// r.HandleFunc("/create/comment", middleWare.AuthenticationUser(handlerComment.CreateComment)).Methods("PUT")
-	// r.HandleFunc("/register", handlerUser.Register).Methods("POST")
-	// r.HandleFunc("/login", handlerUser.Login).Methods("POST")
-
-	// srv := &http.Server{
-	// 	Handler: r,
-	// 	Addr:    "127.0.0.1:7000",
-	// 	// Good practice: enforce timeouts for servers you create!
-	// 	WriteTimeout: 5 * time.Second,
-	// 	ReadTimeout:  5 * time.Second,
-	// }
-
-	// handlerCors := c.Handler(r)
-
-	// log.Fatal(srv.ListenAndServe(), handlerCors)
+	log.Fatal(srv.ListenAndServe(), handlerCors)
 
 }
